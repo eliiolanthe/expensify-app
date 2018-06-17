2cb11b41680dbf2418c507e368136991b72efd2a
Elvira Paulin <eli.paulin@gmail.com>
2018-06-17 19:52:54 +0200

    Added Expenses summary component

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import numeral from 'numeral';
+import { connect } from 'react-redux';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from '../selectors/expenses-total';
+
+const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal).format('$0,0.00');
+    return (
+        <div>
+            <h2>Viewing {`${expenseCount} ${expenseWord}`} totalling {formattedExpensesTotal}</h2>
+        </div>
+    );
+};
+
+const mapStateToProps = (state) => {
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
+    return {
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
+    }
+};
+export default connect(mapStateToProps)(ExpensesSummary);
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,11 +11,6 @@ const Header = () => (
       <li>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
       </li>
-      {/*
-      <li>
-        <NavLink to="/edit" activeClassName="is-active">Edit expense</NavLink>
-      </li>
-      */}
       <li>
         <NavLink to="/help" activeClassName="is-active">Help</NavLink>
       </li>
